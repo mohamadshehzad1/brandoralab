e6f88d2c4223cccf23d7bed3ccf758e65e1a4709
mohamadshehzad1 <brandoralab@gmail.com>
2026-02-12 11:24:49 +0000

    Add Ezoic ads.txt redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   images: {
-    unoptimized: true, 
+    unoptimized: true,
     domains: ['res.cloudinary.com'],
+  },
+
+  // ----------- Ezoic Ads.txt Redirect -----------
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/19390/brandoralab.com',
+        permanent: true,
+      },
+    ];
   },
 };
 
